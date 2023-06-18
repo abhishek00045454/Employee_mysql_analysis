eb2a9112b4b3816c274b6a5bac52c7c6216670b8
ABHISHEK SAINI <113900897+abhishek00045454@users.noreply.github.com>
2023-06-18 22:27:11 +0530

    Add files via upload

---
+++ HR Data Questions.sql
@@ -0,0 +1,29 @@
+-- QUESTIONS
+
+-- 1. What is the gender breakdown of employees in the company?
+
+-- 2. What is the race/ethnicity breakdown of employees in the company?
+
+-- 3. What is the age distribution of employees in the company?
+
+
+-- 4. How many employees work at headquarters versus remote locations?
+
+
+-- 5. What is the average length of employment for employees who have been terminated?
+
+-- 6. How does the gender distribution vary across departments and job titles?
+
+
+-- 7. What is the distribution of job titles across the company?
+
+
+-- 8. Which department has the highest turnover rate?
+
+
+-- 9. What is the distribution of employees across locations by city and state?
+
+
+-- 10. How has the company's employee count changed over time based on hire and term dates?
+
+-- 11. What is the tenure distribution for each department?
